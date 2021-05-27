9b5ed212653ce7bc18ea3dd23650e61e7b10cf70
Nuel-C <eniemmanuel.c@gmail.com>
2021-05-27 16:08:05 +0100

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log('Api Running')
+})
